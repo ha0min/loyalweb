fb5503190c4b153839c03a9add1cd4c94b5da09d
Max Cheng <ofcoursemax@gmail.com>
2023-11-05 14:34:10 -0800

    feat: add common type

---
+++ type.ts
@@ -0,0 +1,22 @@
+'use client';
+
+export type ProductDetail = {
+    category?: string;
+    description?: string;
+    id?: number;
+    name?: string;
+    picture?: string;
+    price?: number;
+    stock?: number;
+};
+
+export type User = {
+    name?: string,
+    email?: string,
+    avatar?: string,
+    phone?: string,
+    address?: string,
+    userRole?: string,
+    profile?: string,
+    points?: number,
+};
