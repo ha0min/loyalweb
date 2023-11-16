f3b486f38d16cc29ecec382dd80d89769ae3ee2b
Max Cheng <ofcoursemax@gmail.com>
2023-11-15 21:22:13 -0800

    style: update the color theme

--- theme.ts
+++ theme.ts
@@ -1,7 +1,23 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import {createTheme, MantineColorsTuple} from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+    '#f6ecff',
+    '#e7d6fb',
+    '#caabf1',
+    '#ac7ce8',
+    '#9354e0',
+    '#833cdb',
+    '#7b2eda',
+    '#6921c2',
+    '#5d1cae',
+    '#501599',
+];
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+    /* Put your mantine theme override here */
+    colors: {
+        myColor,
+    },
 });
